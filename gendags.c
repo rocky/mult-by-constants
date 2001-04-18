5c1b4aade9eef0042e00877eb9489970cca20ce2
lefevre <empty>
2001-04-18 14:36:05 +0000

    Recherche simple de DAGs isomorphes.

--- gendags.c
+++ gendags.c
@@ -1,5 +1,5 @@
 /*
- * $Id$
+ * $Id: gendags.c 1.1 2001/04/17 19:37:48 lefevre Exp lefevre $
  *
  * In order to find the possible results (in an interval) that can be
  * obtained with q shift-and-add operations, this program generates a
@@ -11,8 +11,19 @@
  *   * From each node, there exists a path leading to the target
  *     node (i.e., there are no useless nodes).
  *
- * Usage: gendags <q>
+ * Usage: gendags <level> <q>
  *   q: number of nodes + 1 (= number of elementary operations)
+ *   level:
+ *     0 -> pairs must be ordered.
+ *     1 -> level 0 + simple isomorphism search
+ *   e.g.
+ *     $ diff <(./gendags 0 4) <(./gendags 1 4)
+ *     3d2
+ *     < (0,0) (0,0) (2,0) (3,1)
+ *     6d4
+ *     < (0,0) (0,0) (2,1) (3,2)
+ *     8d5
+ *     < (0,0) (0,0) (2,2) (3,1)
  */
 
 #include <stdio.h>
@@ -24,25 +35,33 @@
 
 int main(int argc, char **argv)
 {
+  int level;
   int q;
   int *dag;
 
-  if (argc != 2)
+  if (argc != 3)
   {
-    fprintf(stderr, "Usage: gendags <q>\n");
+    fprintf(stderr, "Usage: gendags <level> <q>\n");
     exit(1);
   }
 
-  q = atoi(argv[1]);
+  level = atoi(argv[1]);
+  if (level < 0)
+  {
+    fprintf(stderr, "gendags: level must not be negative\n");
+    exit(2);
+  }
+
+  q = atoi(argv[2]);
   if (q < 1)
   {
     fprintf(stderr, "gendags: q must be at least 1\n");
-    exit(2);
+    exit(3);
   }
   if (q > 32)
   {
     fprintf(stderr, "gendags: q must be at most 32\n");
-    exit(3);
+    exit(4);
   }
   if (q == 1)
   {
@@ -54,13 +73,14 @@ int main(int argc, char **argv)
   if (dag == NULL)
   {
     fprintf(stderr, "sdags: out of memory!\n");
-    exit(4);
+    exit(5);
   }
 
   while (1)
   {
     unsigned long u;
 
+  nextdag:
     do
     {
       int i, j, x, y;
@@ -88,7 +108,7 @@ int main(int argc, char **argv)
       while (i > 2);
       return 0;
 
-      inc_ok:
+    inc_ok:
       assert(even(i));
       u = (1 << x) | (1 << y);
       for (j = 0; j < i; j++)
@@ -101,6 +121,38 @@ int main(int argc, char **argv)
       }
     }
     while (u != (1UL << q) - 1);
+
+    if (level)
+    {
+      int i;
+
+      for (i = 0; i < q-2; i++)
+      {
+        int j, x, y;
+
+        x = dag[2*i];
+        y = dag[2*i+1];
+        j = i;
+        do
+        {
+          j++;
+          assert(j < q);  /* because the last pair contains q-1 and
+                           * is different from the other ones */
+        }
+        while (dag[2*j] == x && dag[2*j+1] == y);
+        if (j - i > 1)  /* number of identical pairs */
+          while (j < q)
+          {
+            unsigned long v;
+            v = (((1 << dag[2*j]) | (1 << dag[2*j+1])) >> (i+1)) & 3;
+            if (v == 1)  /* 1st pair referenced, not the 2nd -> OK */
+              break;
+            if (v == 2)  /* 2nd pair referenced, not the 1st -> reject */
+              goto nextdag;
+            j++;
+          }
+      }  /* loop i */
+    }  /* if (level) */
 
     {
       int i;
