edd5756645f2f971f491b1e0e2459cd6f08899f6
lefevre <empty>
2002-12-12 09:24:05 +0000

    Style GNU.

--- gendags.c
+++ gendags.c
@@ -1,5 +1,5 @@
 /*
- * $Id: gendags.c 1.1 2001/04/17 19:37:48 lefevre Exp lefevre $
+ * $Id: gendags.c 1.2 2001/04/18 14:36:05 lefevre Exp lefevre $
  *
  * In order to find the possible results (in an interval) that can be
  * obtained with q shift-and-add operations, this program generates a
@@ -40,124 +40,124 @@ int main(int argc, char **argv)
   int *dag;
 
   if (argc != 3)
-  {
-    fprintf(stderr, "Usage: gendags <level> <q>\n");
-    exit(1);
-  }
+    {
+      fprintf(stderr, "Usage: gendags <level> <q>\n");
+      exit(1);
+    }
 
   level = atoi(argv[1]);
   if (level < 0)
-  {
-    fprintf(stderr, "gendags: level must not be negative\n");
-    exit(2);
-  }
+    {
+      fprintf(stderr, "gendags: level must not be negative\n");
+      exit(2);
+    }
 
   q = atoi(argv[2]);
   if (q < 1)
-  {
-    fprintf(stderr, "gendags: q must be at least 1\n");
-    exit(3);
-  }
+    {
+      fprintf(stderr, "gendags: q must be at least 1\n");
+      exit(3);
+    }
   if (q > 32)
-  {
-    fprintf(stderr, "gendags: q must be at most 32\n");
-    exit(4);
-  }
+    {
+      fprintf(stderr, "gendags: q must be at most 32\n");
+      exit(4);
+    }
   if (q == 1)
-  {
-    printf("(0,0)\n");
-    return 0;
-  }
+    {
+      printf("(0,0)\n");
+      return 0;
+    }
 
   dag = calloc(2*q, sizeof(int));
   if (dag == NULL)
-  {
-    fprintf(stderr, "sdags: out of memory!\n");
-    exit(5);
-  }
+    {
+      fprintf(stderr, "sdags: out of memory!\n");
+      exit(5);
+    }
 
   while (1)
-  {
-    unsigned long u;
-
-  nextdag:
-    do
     {
-      int i, j, x, y;
+      unsigned long u;
 
-      i = 2*q - 1;
+    nextdag:
       do
-      {
-        assert(odd(i) && dag[i] <= dag[i-1]);
-        if (dag[i] != dag[i-1])
         {
-          y = ++dag[i];
-          x = dag[--i];
-          goto inc_ok;
-        }
-        i--;
-        assert(even(i) && dag[i] <= i/2);
-        if (dag[i] != i/2)
-        {
-          x = ++dag[i];
-          y = 0;
-          goto inc_ok;
-        }
-        i--;
-      }
-      while (i > 2);
-      return 0;
+          int i, j, x, y;
 
-    inc_ok:
-      assert(even(i));
-      u = (1 << x) | (1 << y);
-      for (j = 0; j < i; j++)
-        u |= 1 << dag[j];
-      while (i < 2*q)
+          i = 2*q - 1;
+          do
+            {
+              assert(odd(i) && dag[i] <= dag[i-1]);
+              if (dag[i] != dag[i-1])
+                {
+                  y = ++dag[i];
+                  x = dag[--i];
+                  goto inc_ok;
+                }
+              i--;
+              assert(even(i) && dag[i] <= i/2);
+              if (dag[i] != i/2)
+                {
+                  x = ++dag[i];
+                  y = 0;
+                  goto inc_ok;
+                }
+              i--;
+            }
+          while (i > 2);
+          return 0;
+
+        inc_ok:
+          assert(even(i));
+          u = (1 << x) | (1 << y);
+          for (j = 0; j < i; j++)
+            u |= 1 << dag[j];
+          while (i < 2*q)
+            {
+              dag[i] = x;
+              dag[i+1] = y;
+              i += 2;
+            }
+        }
+      while (u != (1UL << q) - 1);
+
+      if (level)
+        {
+          int i;
+
+          for (i = 0; i < q-2; i++)
+            {
+              int j, x, y;
+
+              x = dag[2*i];
+              y = dag[2*i+1];
+              j = i;
+              do
+                {
+                  j++;
+                  assert(j < q);  /* because the last pair contains q-1 and
+                                     is different from the other ones */
+                }
+              while (dag[2*j] == x && dag[2*j+1] == y);
+              if (j - i > 1)  /* number of identical pairs */
+                while (j < q)
+                  {
+                    unsigned long v;
+                    v = (((1 << dag[2*j]) | (1 << dag[2*j+1])) >> (i+1)) & 3;
+                    if (v == 1)  /* 1st pair referenced, not the 2nd -> OK */
+                      break;
+                    if (v == 2)  /* 2nd pair referenced, not the 1st -> rej */
+                      goto nextdag;
+                    j++;
+                  }
+            }  /* loop i */
+        }  /* if (level) */
+
       {
-        dag[i] = x;
-        dag[i+1] = y;
-        i += 2;
+        int i;
+        for (i = 0; i < q; i++)
+          printf("(%d,%d)%s", dag[2*i], dag[2*i+1], i < q - 1 ? " " : "\n");
       }
     }
-    while (u != (1UL << q) - 1);
-
-    if (level)
-    {
-      int i;
-
-      for (i = 0; i < q-2; i++)
-      {
-        int j, x, y;
-
-        x = dag[2*i];
-        y = dag[2*i+1];
-        j = i;
-        do
-        {
-          j++;
-          assert(j < q);  /* because the last pair contains q-1 and
-                           * is different from the other ones */
-        }
-        while (dag[2*j] == x && dag[2*j+1] == y);
-        if (j - i > 1)  /* number of identical pairs */
-          while (j < q)
-          {
-            unsigned long v;
-            v = (((1 << dag[2*j]) | (1 << dag[2*j+1])) >> (i+1)) & 3;
-            if (v == 1)  /* 1st pair referenced, not the 2nd -> OK */
-              break;
-            if (v == 2)  /* 2nd pair referenced, not the 1st -> reject */
-              goto nextdag;
-            j++;
-          }
-      }  /* loop i */
-    }  /* if (level) */
-
-    {
-      int i;
-      for (i = 0; i < q; i++)
-        printf("(%d,%d)%s", dag[2*i], dag[2*i+1], i < q - 1 ? " " : "\n");
-    }
-  }
 }
