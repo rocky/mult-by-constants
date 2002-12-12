0f8c7fb8742a71269362ed23c7da85db29e3eb89
lefevre <empty>
2002-12-12 09:17:09 +0000

    Style GNU + changement des messages d'erreur.

--- dagsearch.c
+++ dagsearch.c
@@ -1,5 +1,5 @@
 /*
- * $Id: dagsearch.c 1.8 2001/05/04 00:26:33 lefevre Exp lefevre $
+ * $Id: dagsearch.c 1.9 2001/05/04 08:04:11 lefevre Exp lefevre $
  *
  * Usage: dagsearch <mrec> <mmax> <file>
  *   mrec: maximum recorded value
@@ -59,71 +59,71 @@ void dagsearch(int q, int *dag, unsigned char *cost, long mrec, long mmax)
   dag -= 2;
   v[0] = 1;
   for (i = 0; ;)
-  {
-    if (i == q)
     {
-    next:
-      s[i]++;
-      x[2*i+r[i]] <<= 1;
-      v[i] = o[i] ? DIST(x[2*i], x[2*i+1]) : x[2*i] + x[2*i+1];
-    }
-    else
-    {
-      i++;
-      x[2*i] = v[dag[2*i]];
-      x[2*i+1] = v[dag[2*i+1]];
-      o[i] = 0;
-      r[i] = 0;
-      s[i] = 0;
-      v[i] = x[2*i] + x[2*i+1];
-    }
-    while (v[i] > mmax)
-    {
-      x[2*i] = v[dag[2*i]];
-      x[2*i+1] = v[dag[2*i+1]];
-      if (r[i] == 0 && s[i])
-      {
-        x[2*i+1] <<= 1;
-        r[i] = 1;
-        s[i] = 1;
-        v[i] = o[i] ? DIST(x[2*i], x[2*i+1]) : x[2*i] + x[2*i+1];
-      }
-      else if (o[i] == 0)
-      {
-        o[i] = 1;
-        r[i] = 0;
-        s[i] = 0;
-        v[i] = DIST(x[2*i], x[2*i+1]);
-      }
-      else if (--i)
-        goto next;
+      if (i == q)
+        {
+        next:
+          s[i]++;
+          x[2*i+r[i]] <<= 1;
+          v[i] = o[i] ? DIST(x[2*i], x[2*i+1]) : x[2*i] + x[2*i+1];
+        }
       else
-        return;
+        {
+          i++;
+          x[2*i] = v[dag[2*i]];
+          x[2*i+1] = v[dag[2*i+1]];
+          o[i] = 0;
+          r[i] = 0;
+          s[i] = 0;
+          v[i] = x[2*i] + x[2*i+1];
+        }
+      while (v[i] > mmax)
+        {
+          x[2*i] = v[dag[2*i]];
+          x[2*i+1] = v[dag[2*i+1]];
+          if (r[i] == 0 && s[i])
+            {
+              x[2*i+1] <<= 1;
+              r[i] = 1;
+              s[i] = 1;
+              v[i] = o[i] ? DIST(x[2*i], x[2*i+1]) : x[2*i] + x[2*i+1];
+            }
+          else if (o[i] == 0)
+            {
+              o[i] = 1;
+              r[i] = 0;
+              s[i] = 0;
+              v[i] = DIST(x[2*i], x[2*i+1]);
+            }
+          else if (--i)
+            goto next;
+          else
+            return;
+        }
+      if (v[i] == 0)
+        goto next;
+      if (i < q)
+        {
+          int j;
+          for (j = 0; j < i; j++)
+            {
+              VALUE x;
+              x = v[j];
+              while (x < v[i])
+                x <<= 1;
+              if (x == v[i])
+                goto next;
+            }
+        }
+      OUT("%3d: %3" VALUEFMT "  ( %3" VALUEFMT " %3" VALUEFMT " )",
+          i, v[i], x[2*i], x[2*i+1]);
+      if (v[i] <= mrec && cost[v[i]] > i)
+        {
+          cost[v[i]] = i;
+          OUT("   -   Cost(%3" VALUEFMT ") = %d", v[i], i);
+        }
+      OUT("\n");
     }
-    if (v[i] == 0)
-      goto next;
-    if (i < q)
-    {
-      int j;
-      for (j = 0; j < i; j++)
-      {
-        VALUE x;
-        x = v[j];
-        while (x < v[i])
-          x <<= 1;
-        if (x == v[i])
-          goto next;
-      }
-    }
-    OUT("%3d: %3" VALUEFMT "  ( %3" VALUEFMT " %3" VALUEFMT " )",
-        i, v[i], x[2*i], x[2*i+1]);
-    if (v[i] <= mrec && cost[v[i]] > i)
-    {
-      cost[v[i]] = i;
-      OUT("   -   Cost(%3" VALUEFMT ") = %d", v[i], i);
-    }
-    OUT("\n");
-  }
 }
 
 int main(int argc, char **argv)
@@ -133,109 +133,108 @@ int main(int argc, char **argv)
   unsigned char *cost;
 
   if (argc != 4)
-  {
-    fprintf(stderr, "Usage: dagsearch <mrec> <mmax> <file>\n");
-    exit(1);
-  }
+    {
+      fprintf(stderr, "Usage: dagsearch <mrec> <mmax> <file>\n");
+      exit(1);
+    }
 
   mrec = STRTOVALUE(argv[1]);
   if (errno == ERANGE)
-  {
-    fprintf(stderr, "dagsearch: mrec is too large (out of range)\n");
-    exit(3);
-  }
+    {
+      fprintf(stderr, "dagsearch: mrec is too large (out of range)\n");
+      exit(3);
+    }
   if (mrec < 1)
-  {
-    fprintf(stderr, "dagsearch: mrec must be at least 1\n");
-    exit(2);
-  }
+    {
+      fprintf(stderr, "dagsearch: mrec must be at least 1\n");
+      exit(2);
+    }
 
   mmax = STRTOVALUE(argv[2]);
   if (errno == ERANGE)
-  {
-    fprintf(stderr, "dagsearch: mmax is too large (out of range)\n");
-    exit(5);
-  }
+    {
+      fprintf(stderr, "dagsearch: mmax is too large (out of range)\n");
+      exit(5);
+    }
   if (mmax < mrec)
-  {
-    fprintf(stderr, "dagsearch: mmax must be greater or equal to mrec\n");
-    exit(4);
-  }
+    {
+      fprintf(stderr, "dagsearch: mmax must be greater or equal to mrec\n");
+      exit(4);
+    }
 
   cost = malloc(mrec+1);
   if (cost == NULL)
-  {
-    fprintf(stderr, "dagsearch: out of memory!\n");
-    exit(10);
-  }
+    {
+      fprintf(stderr, "dagsearch: out of memory!\n");
+      exit(10);
+    }
   memset(cost, -1, mrec+1);
   cost[0] = 0;
   cost[1] = 0;
 
   while (1)
-  {
-    VALUE v;
-    int q;
-    int dag[2*QMAX];
-    char buffer[8];
-    FILE *f;
-
-    line++;
-    for (q = 0; scanf("%1[\n]", buffer) == 0; q++)
     {
-      if (q > QMAX)
-      {
-        fprintf(stderr, "dagsearch: too many nodes line %lu\n", line);
-        exit(6);
-      }
-      if (scanf("(%7[(),0-9])", buffer) != 1 ||
-          sscanf(buffer, "%d,%d", dag+2*q, dag+2*q+1) != 2)
-      {
-        fprintf(stderr, "dagsearch: input error line %lu (bad format?)\n",
-                line);
-        exit(7);
-      }
-      scanf("%*[\t ]");
-    }
+      VALUE v;
+      int q;
+      int dag[2*QMAX];
+      char buffer[8];
+      FILE *f;
 
-    if (!q)
-    {
-      if (feof(stdin))
-        return 0;
-      fprintf(stderr, "dagsearch: input error line %lu (bad format?)\n",
-              line);
-      exit(8);
-    }
+      line++;
+      for (q = 0; scanf("%1[\n]", buffer) == 0; q++)
+        {
+          if (q > QMAX)
+            {
+              fprintf(stderr, "dagsearch: too many nodes at line %lu\n",
+                      line);
+              exit(6);
+            }
+          if (scanf("(%7[(),0-9])", buffer) != 1 ||
+              sscanf(buffer, "%d,%d", dag+2*q, dag+2*q+1) != 2)
+            {
+              fprintf(stderr, "dagsearch: input error at line %lu\n", line);
+              exit(7);
+            }
+          scanf("%*[\t ]");
+        }
 
-    dagsearch(q, dag, cost, mrec, mmax);
+      if (!q)
+        {
+          if (feof(stdin))
+            return 0;
+          fprintf(stderr, "dagsearch: input error at line %lu\n", line);
+          exit(8);
+        }
 
-    for (v = 1; v < mrec; v += 2)
-    {
-      VALUE w;
-      int c;
+      dagsearch(q, dag, cost, mrec, mmax);
 
-      c = cost[v];
-      w = v;
-      while ((w <<= 1) <= mrec)
-        if (cost[w] > c)
-          cost[w] = c;
-    }
+      for (v = 1; v < mrec; v += 2)
+        {
+          VALUE w;
+          int c;
 
-    f = fopen(argv[3], "wb");
-    if (f == NULL)
-    {
-      fprintf(stderr, "dagsearch: cannot create file\n");
-      exit(11);
+          c = cost[v];
+          w = v;
+          while ((w <<= 1) <= mrec)
+            if (cost[w] > c)
+              cost[w] = c;
+        }
+
+      f = fopen(argv[3], "wb");
+      if (f == NULL)
+        {
+          fprintf(stderr, "dagsearch: cannot create file %s\n", argv[3]);
+          exit(11);
+        }
+      if (fwrite(cost, mrec+1, 1, f) != 1)
+        {
+          fprintf(stderr, "dagsearch: cannot write to file %s\n", argv[3]);
+          exit(12);
+        }
+      if (fclose(f))
+        {
+          fprintf(stderr, "dagsearch: cannot close file %s\n", argv[3]);
+          exit(13);
+        }
     }
-    if (fwrite(cost, mrec+1, 1, f) != 1)
-    {
-      fprintf(stderr, "dagsearch: cannot write to file\n");
-      exit(12);
-    }
-    if (fclose(f))
-    {
-      fprintf(stderr, "dagsearch: cannot close file\n");
-      exit(13);
-    }
-  }
 }
