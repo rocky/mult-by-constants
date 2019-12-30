4f9db0a27cb8ee6a84aeb53288a960b58bec7d0a
rocky <rb@dustyfeet.com>
2019-12-30 08:48:43 -0500

    Expand short-factor test

--- python/pytest/test_20_search_short_factors.py
+++ python/pytest/test_20_search_short_factors.py
@@ -37,20 +37,18 @@ def test_factor():
     )
 
     check(n, s_cost, s_instrs)
-    # cost, instrs = mconst.try_shift_op_factor(n, 5, "add", 2, bin_cost, 0, [], bin_instrs)
-    # assert bin_cost == cost, "5 is not a factor of 27, so we keep old bin_cost"
+    cost, instrs = mconst.try_shift_op_factor(n, 5, "add", 2, s_cost, 0, [], s_instrs)
+    assert s_cost == cost, "5 is not a factor of 27, so we keep old s_cost"
 
-    # for n, factor, shift_amount in ((27, 3, 1), (85, 5, 2)):
-    #     bin_cost, bin_instrs = binary_method.binary_sequence(mconst, n)
-    #     if debug:
-    #         print_instructions(bin_instrs, n, bin_cost)
-    #     result = []
-    #     cost, result = mconst.try_shift_op_factor(
-    #         n, factor, "add", shift_amount, bin_cost, 0, [], bin_instrs
-    #     )
-    #     assert cost < bin_cost, f"should use the fact that {factor} is a factor of {n}"
-    #     if debug:
-    #         print_instructions(result, n, cost)
+    for n, factor, shift_amount in ((51, 3, 1), (85, 5, 2)):
+        s_cost, s_instrs = binary_method.binary_sequence(mconst, n)
+        print_instructions(s_instrs, n, s_cost)
+        result = []
+        cost, result = mconst.try_shift_op_factor(
+            n, factor, "add", shift_amount, s_cost, 0, [], s_instrs
+        )
+        assert cost < s_cost, f"should use the fact that {factor} is a factor of {n}"
+        print_instructions(result, n, cost)
 
 
 # If run as standalone
